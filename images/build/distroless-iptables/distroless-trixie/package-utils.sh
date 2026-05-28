25f5a9ab4446ad6504e2e3c4d8d8935e571278f4
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2026-05-28 23:31:49 +0800

    Fix usrmerge COPY conflict when staging binaries from /bin and /sbin
    Debian 13+ (usrmerge) makes /bin and /sbin symlinks to /usr/bin and
    /usr/sbin. When stage_file copies binaries into the stage directory
    without rewriting these paths, the subsequent COPY --from=build into a
    usrmerge-based intermediate image fails because Docker cannot merge a
    directory over a symlink. Remap /bin and /sbin paths to /usr/bin and
    /usr/sbin, matching the existing /lib -> /usr/lib handling.

--- images/build/distroless-iptables/distroless-trixie/package-utils.sh
+++ images/build/distroless-iptables/distroless-trixie/package-utils.sh
@@ -36,6 +36,10 @@ stage_file() {
     from="${1}"
     if [[ $from = /lib*/* ]]; then
         from="/usr$from"
+    elif [[ $from = /bin/* ]]; then
+        from="/usr$from"
+    elif [[ $from = /sbin/* ]]; then
+        from="/usr$from"
     fi
     cp -a --parents "${from}" "${2}"
     # recursively follow symlinks
